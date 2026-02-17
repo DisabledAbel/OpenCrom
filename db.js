5816567fd4cd489bd661f817ce6211cb05220d11
Abel <fazea4930@gmail.com>
2026-02-16 17:41:41 -0800

    Initialize database connection with pg Pool

---
+++ db.js
@@ -0,0 +1,9 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("render")
+    ? { rejectUnauthorized:false }
+    : false
+});
